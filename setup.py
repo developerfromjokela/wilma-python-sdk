e5c5c2e9d5080435aa350d2e705a7c3587d5f54b
developerfromjokela <developerfromjokela@gmail.com>
2020-11-10 10:58:09 +0200

    Bumped up version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='wilma-python-sdk',
-    version='0.1.5',
+    version='0.1.6',
     author="Developer From Jokela",
     author_email="info@developerfromjokela.com",
     description="Python SDK for Wilma API",
