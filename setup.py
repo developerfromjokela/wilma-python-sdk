a3bdf8dae8c299b397729f4b5000f8c38b4e4068
Developer From Jokela <40487414+developerfromjokela@users.noreply.github.com>
2023-01-30 21:03:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='wilma-python-sdk',
-    version='0.3.7',
+    version='0.3.8',
     author="Developer From Jokela",
     author_email="info@developerfromjokela.com",
     description="Python SDK for Wilma API",
